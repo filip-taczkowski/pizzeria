cd65d591f6462a3d845ea3e45c4acf24d430dd2b
Filip Taczkowski <filip.taczkowski@gmail.com>
2019-11-14 18:03:48 +0100

    Add getData method to Booking class

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,7 +1,9 @@
-import { templates, select } from '../settings.js';
+import { templates, select, settings } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
+import utils from '../utils.js';
+
 
 class Booking {
   constructor(element){
@@ -9,6 +11,7 @@ class Booking {
 
     thisBooking.render(element);
     thisBooking.initWidgets();
+    thisBooking.getData();
 
   }
 
@@ -36,6 +39,64 @@ class Booking {
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker, 1);
     thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker, 1);
 
+  }
+
+  getData(){
+    const thisBooking = this;
+
+
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      booking: [
+        startDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+    };
+
+    //console.log('getData params: ', params);
+
+    const urls = {
+      booking:       settings.db.url + '/' + settings.db.booking + '?'
+                                           + params.booking.join('&') ,
+      eventsCurrent: settings.db.url + '/' + settings.db.event + '?'
+                                           + params.eventsCurrent.join('&'),
+      eventsRepeat:  settings.db.url + '/' + settings.db.event + '?'
+                                           + params.eventsRepeat.join('&'),
+    };
+
+    //console.log('getData url: ', urls);
+
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(allResponses){
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });
   }
 }
 
